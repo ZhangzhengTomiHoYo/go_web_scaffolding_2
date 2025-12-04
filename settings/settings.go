7b1cdad6fa0bcedbb35adc4e853af4ac7194344f
Zheng Zhang <1296775258@qq.com>
2025-12-04 17:06:52 +0800

    1.mod:两种指定配置文件的方式

--- settings/settings.go
+++ settings/settings.go
@@ -48,10 +48,16 @@ type RedisConfig struct {
 }
 
 func Init() (err error) {
-	//viper.SetConfigFile("config.yaml") // 直接指定文件名称
-	viper.SetConfigName("config") // 指定配置文件名称（不需要带后缀）
-	viper.SetConfigType("yaml")   // 指定配置文件类型 (专用于远程，本地不生效)
-	viper.AddConfigPath("./")     // 指定查找配置文件的路径
+	// 方式1: 直接指定配置文件路径 (相对路径或者绝对路径)
+	// 相对路径: 相对执行的可执行文件的相对路径
+	// 绝对路径: 系统中实际的文件路径
+	//viper.SetConfigFile("./conf/config.yaml")
+
+	// 方式2: 指定配置文件名和配置文件的位置, viper自行查找可用的配置文件
+	// 配置文件名不需要带后缀
+	// 配置文件位置可配置多个
+	viper.SetConfigName("config") // 指定配置文件名 (不带后缀)
+	viper.AddConfigPath(".")      // 指定查找配置文件的路径 (这里使用相对路径)
 	err = viper.ReadInConfig()
 	if err != nil {
 		fmt.Printf("%v\n", err)
